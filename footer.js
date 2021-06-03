97b27e6ab53f3e2b65b6be56ae9acd79cc311297
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-06-03 18:45:16 -0400

    homework

---
+++ footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+
+
+class Footer extends React.Component
+{
+  render()
+  {
+    return (
+      <nav class="navbar navbar-expand-sm bg-white">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link links" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="#">Add Attractions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="#">View Attractions</a>
+        </li>
+      </ul>
+    </nav>
+
+
+    )
+  }
+}
+export default Footer;
